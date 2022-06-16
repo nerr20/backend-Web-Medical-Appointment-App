ab98333cd2460a2a6aa2f6be708d66eedf55c12a
nerr20 <104547296+nerr20@users.noreply.github.com>
2022-06-16 19:16:43 +0800

    Update views.py

--- announcement/views.py
+++ announcement/views.py
@@ -7,6 +7,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from .filters import AnnouncementFilter
 
+from rest_framework import status
+from rest_framework.response import Response
+from collections import OrderedDict
 
 class AnnouncementModelViewSet(ModelViewSet):
     queryset = Announcement.objects.all()
@@ -21,3 +24,18 @@ class AnnouncementModelViewSet(ModelViewSet):
     filterset_class = AnnouncementFilter
     search_fields = ['title', 'body']
     ordering_fields = ['created_time', 'modified_time']
+    
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        ret = OrderedDict(dict(id=response.data.pop('id')))
+        return Response(ret, status=response.status_code, headers=response.headers)
+
+    def retrieve(self, request, *args, **kwargs):
+        response = super().retrieve(request, *args, **kwargs)
+        ret = response.data.copy()
+        ret.pop('id')
+        return Response(ret)
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        return Response(status=status.HTTP_204_NO_CONTENT)
